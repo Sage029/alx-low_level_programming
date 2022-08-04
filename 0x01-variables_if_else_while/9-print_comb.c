5246ec2c75b326fa01247bab641b471a270cc9a9
Mubaraq <babatundemubaraq1650@gmail.com>
2022-08-04 12:01:20 -0700

    Add stuff

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-	int a;i
+	char a;
 
-	a = 0;
-	while (a <= 9)
+	a = '0';
+	while (a <= '9')
 	{
 		putchar(a);
-		if (a != 9)
+		if (a != '9')
 		{
 			putchar(',');
 			putchar(' ');
